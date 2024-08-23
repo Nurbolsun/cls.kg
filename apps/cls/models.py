ccd02282c9445bd05335b81f93b432d506dc91a4
Nurbolsun <100375647+Nurbolsun@users.noreply.github.com>
2024-08-23 21:54:15 +0600

    lp raiting

--- apps/cls/models.py
+++ apps/cls/models.py
@@ -33,12 +33,23 @@ class Branch(models.Model):
     region = models.ForeignKey(Region, on_delete=models.CASCADE, verbose_name="Район")
     address = models.CharField(max_length=100, verbose_name='Адрес', blank=False)
     time_work = models.CharField(max_length=100, verbose_name='Время работы', blank=False)
+    video_link = models.CharField(max_length=100, verbose_name='Видео ссылка', blank=True)
+    audio = models.FileField(upload_to='audio/', verbose_name='Аудио', blank=True, null=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00, verbose_name="Рейтинг")
+    num_votes = models.PositiveIntegerField(default=0, verbose_name="Количество голосов")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
     is_active = models.BooleanField(verbose_name="Активный", default=True)
 
     def __str__(self):
         return self.name
+
+    def update_rating(self, new_rating):
+        total_rating = self.rating * self.num_votes
+        total_rating += new_rating
+        self.num_votes += 1
+        self.rating = total_rating / self.num_votes
+        self.save()
 
     class Meta:
         verbose_name = "Филиал"
